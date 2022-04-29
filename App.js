08ee47b956451c40743aa7d3d666a82f88031b33
Denisa Haka <denisa.haka@ymail.com>
2022-04-29 14:39:08 +0100

    fixed bug files encoding

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
+import react from "react";
+
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { useFonts } from "expo-font";
 
+import Home from "./screens/Home";
+import Details from "./screens/Details";
 
 //stacks = routes
 const Stack = createStackNavigator();
@@ -10,12 +14,24 @@ const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    background:'trasparent'
+    background:'transparent'
   }
 }
 
 
-export default function App() {
+const App = () => {
+
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if (!loaded) return null;
+
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName="Home">
@@ -28,4 +44,6 @@ export default function App() {
   );
 }
 
+
+export default App;
 
